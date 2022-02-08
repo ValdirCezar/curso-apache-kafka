9a906eece450275a29965056969d73d78e59aa00
Valdir Cezar de Jesus <valdir@MacBook-Air-de-Valdir.local>
2022-02-08 20:44:20 -0300

    str-producer started

---
+++ str-producer/src/main/java/com/valdir/strproducer/StrProducerApplication.java
@@ -0,0 +1,13 @@
+package com.valdir.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrProducerApplication.class, args);
+    }
+
+}
