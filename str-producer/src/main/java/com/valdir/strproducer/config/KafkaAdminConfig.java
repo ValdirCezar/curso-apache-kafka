0c5b1ef30365045b2a90127c251ee843e0b7b2ab
Valdir Cezar de Jesus <valdir@MacBook-Air-de-Valdir.local>
2022-02-09 17:48:30 -0300

    configure kafka admin

---
+++ str-producer/src/main/java/com/valdir/strproducer/config/KafkaAdminConfig.java
@@ -0,0 +1,32 @@
+package com.valdir.strproducer.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@Configuration
+public class KafkaAdminConfig {
+
+    public final KafkaProperties properties;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics topics() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("str-topic").partitions(2).replicas(1).build()
+        );
+    }
+}
