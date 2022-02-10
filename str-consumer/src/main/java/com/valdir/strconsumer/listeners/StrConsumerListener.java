19cd239f227582c0473c60648e5c0ca999f7c379
Valdir Cezar de Jesus <valdir@MacBook-Air-de-Valdir.local>
2022-02-10 19:41:17 -0300

    defining partitions for consumers

--- str-consumer/src/main/java/com/valdir/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/valdir/strconsumer/listeners/StrConsumerListener.java
@@ -2,18 +2,27 @@ package com.valdir.strconsumer.listeners;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                @TopicPartition(topic = "str-topic", partitions = {"0"})
+            }
+            , containerFactory = "strContainerFactory")
     public void create(String message) {
         log.info("CREATE ::: Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                @TopicPartition(topic = "str-topic", partitions = {"1"})
+            }
+            , containerFactory = "strContainerFactory")
     public void log(String message) {
         log.info("LOG ::: Receive message {}", message);
     }
