656663fbac7e8b2174b5229d8f9bdb26aaa979d4
Valdir Cezar de Jesus <valdir@MacBook-Air-de-Valdir.local>
2022-02-10 16:37:57 -0300

    add calback function to sendMessage function

--- str-producer/src/main/java/com/valdir/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/valdir/strproducer/services/StringProducerService.java
@@ -1,9 +1,11 @@
 package com.valdir.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -11,6 +13,16 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if(success != null) {
+                        log.info("Send message with success {}", message);
+                        log.info("Partition {}, Offset {}",
+                                success.getRecordMetadata().partition(),
+                                success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error send message")
+        );
     }
 }
