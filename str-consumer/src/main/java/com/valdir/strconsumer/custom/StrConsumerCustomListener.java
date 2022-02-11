d5cb8186a9f984d86a2882079441a95b0aeeb1f1
Valdir Cezar de Jesus <valdir@MacBook-Air-de-Valdir.local>
2022-02-11 08:35:28 -0300

    custom listener

---
+++ str-consumer/src/main/java/com/valdir/strconsumer/custom/StrConsumerCustomListener.java
@@ -0,0 +1,25 @@
+package com.valdir.strconsumer.custom;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@KafkaListener
+public @interface StrConsumerCustomListener {
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "topics")
+    String[] topics() default "str-topic";
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "containerFactory")
+    String containerFactory() default "strContainerFactory";
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "groupId")
+    String groupId() default "";
+
+}
