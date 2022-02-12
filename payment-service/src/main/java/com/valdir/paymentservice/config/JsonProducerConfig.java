fec37a1158f0b7b29514332356643b87d8a74260
Valdir Cezar de Jesus <valdir@MacBook-Air-de-Valdir.local>
2022-02-12 14:54:47 -0300

    add producer config

---
+++ payment-service/src/main/java/com/valdir/paymentservice/config/JsonProducerConfig.java
@@ -0,0 +1,36 @@
+package com.valdir.paymentservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@Configuration
+public class JsonProducerConfig {
+
+    private final KafkaProperties properties;
+
+    @Bean
+    public ProducerFactory jsonProducerFactory() {
+        var configs = new HashMap<String, Object>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory(configs, new StringSerializer(), new JsonSerializer());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Serializable> jsonKafkaTemplate(ProducerFactory jsonProducerFactory) {
+        return new KafkaTemplate<>(jsonProducerFactory);
+    }
+}
--- payment-service/src/main/java/com/valdir/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/valdir/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,28 @@ package com.valdir.paymentservice.service.impl;
 
 import com.valdir.paymentservice.model.Payment;
 import com.valdir.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+        log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
